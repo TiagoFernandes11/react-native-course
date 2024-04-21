2456a56df2833ef9c92d0979133ea68a0ed34db5
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-04-21 18:36:04 -0300

    Gerador de numeros

--- NovoProjeto/components/mega/Mega.js
+++ NovoProjeto/components/mega/Mega.js
@@ -1,14 +1,29 @@
 import React from "react";
-import { Text, TextInput, View } from "react-native";
+import { Button, Text, TextInput, View } from "react-native";
 import estilo from "../estilo";
 
 export default class Mega extends React.Component {
   state = {
     qtdeNumeros: this.props.qtdeNumeros,
+    numeros: [],
   };
 
   alterarQtdeNumero(quantidade) {
-    this.setState({ qtdeNumeros: quantidade });
+    this.setState({ qtdeNumeros: +quantidade });
+  }
+
+  gerarNumeros() {
+    qtd = this.state.qtdeNumeros;
+    vetor = [];
+    while (vetor.length < qtd) {
+      temp = Math.round(Math.random() * 100);
+      if (!vetor.includes(temp)) {
+        vetor.push(temp);
+      }
+    }
+    vetor.sort();
+    console.log(vetor)
+    this.setState({ numeros: vetor });
   }
 
   render() {
@@ -21,9 +36,10 @@ export default class Mega extends React.Component {
           keyboardType="numeric"
           placeholder="Qtde de Números"
           style={{ borderBottomWidth: 1 }}
-          value={this.state.qtdeNumeros}
+          value={`${this.state.qtdeNumeros}`}
           onChangeText={(qtde) => this.alterarQtdeNumero(qtde)}
         />
+        <Button title="Generate" onPress={() => this.gerarNumeros()}></Button>
       </View>
     );
   }
