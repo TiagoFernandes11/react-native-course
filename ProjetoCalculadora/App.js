1b374077d98dd4fde9389acad3ad493b516bcda0
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-04-22 15:05:20 -0300

    Display component

--- ProjetoCalculadora/App.js
+++ ProjetoCalculadora/App.js
@@ -1,26 +1,42 @@
 import React from "react";
 import { View, StyleSheet, Text } from "react-native";
 import Button from "./src/components/Button";
-export default function App(){
-  return(
+import Display from "./src/components/Display";
+export default function App() {
+  displayValue = "0";
+
+  return (
     <View style={styles.container}>
+      <Display value={displayValue} />
       <View style={styles.buttons}>
         <Button label="AC"></Button>
+        <Button label="/"></Button>
+        <Button label="7"></Button>
+        <Button label="8"></Button>
+        <Button label="9"></Button>
+        <Button label="*"></Button>
+        <Button label="4"></Button>
+        <Button label="5"></Button>
+        <Button label="6"></Button>
+        <Button label="-"></Button>
+        <Button label="1"></Button>
+        <Button label="2"></Button>
+        <Button label="3"></Button>
+        <Button label="="></Button>
+        <Button label="0"></Button>
+        <Button label="."></Button>
+        <Button label="="></Button>
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
   },
   buttons: {
     flexDirection: "row",
-    flexWrap: "wrap",
-    margin: 10,
+    flexWrap: "wrap"
   },
 });
--- ProjetoCalculadora/src/components/Button.js
+++ ProjetoCalculadora/src/components/Button.js
@@ -1,10 +1,5 @@
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  Dimensions,
-  TouchableHighlight,
-} from "react-native";
+import { StyleSheet, Text, Dimensions, TouchableHighlight } from "react-native";
 
 export default function Button(props) {
   return (
