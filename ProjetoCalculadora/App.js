d3371d4316adfe6d47b0a91da2090b91d06fefc5
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-04-22 19:18:05 -0300

    Fim projeto calculadora

--- ProjetoCalculadora/App.js
+++ ProjetoCalculadora/App.js
@@ -1,5 +1,5 @@
-import React, { Component, useState } from "react";
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
 import Button from "./src/components/Button";
 import Display from "./src/components/Display";
 
@@ -8,18 +8,20 @@ const initialState = {
   clearDisplay: false,
   operation: null,
   values: [0, 0],
-  currentIndex: 0,
+  current: 0,
 };
 
 export default class App extends Component {
   state = { ...initialState };
 
   addDigit = (n) => {
-    if (n === "." && this.state.displayValue.includes(".")) {
-      return;
-    }
     const clearDisplay =
       this.state.displayValue === "0" || this.state.clearDisplay;
+
+    if (n === "." && !clearDisplay && this.state.displayValue.includes(".")) {
+      return;
+    }
+
     const currentValue = clearDisplay ? "" : this.state.displayValue;
     const displayValue = currentValue + n;
     this.setState({ displayValue, clearDisplay: false });
@@ -27,7 +29,7 @@ export default class App extends Component {
     if (n !== ".") {
       const newValue = parseFloat(displayValue);
       const values = [...this.state.values];
-      values[this.state.currentIndex] = newValue;
+      values[this.state.current] = newValue;
       this.setState({ values });
     }
   };
@@ -36,10 +38,33 @@ export default class App extends Component {
     this.setState({ ...initialState });
   };
 
-  setOperation = (operation) => {};
+  setOperation = (operation) => {
+    if (this.state.current === 0) {
+      this.setState({ operation, current: 1, clearDisplay: true });
+    } else {
+      const equals = operation === "=";
+      const values = [...this.state.values];
+      try {
+        values[0] = eval(`${values[0]} ${this.state.operation} ${values[1]}`);
+      } catch (e) {
+        values[0] = this.state.values[0];
+      }
+
+      values[1] = 0;
+      this.setState({
+        displayValue: `${values[0]}`,
+        operation: equals ? null : operation,
+        current: equals ? 0 : 1,
+        //clearDisplay: !equals,
+        clearDisplay: true,
+        values,
+      });
+    }
+  };
+
   render() {
     return (
-      <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
         <Display value={this.state.displayValue} />
         <View style={styles.buttons}>
           <Button label="AC" triple onClick={this.clearMemory} />
@@ -60,7 +85,7 @@ export default class App extends Component {
           <Button label="." onClick={this.addDigit} />
           <Button label="=" operation onClick={this.setOperation} />
         </View>
-      </SafeAreaView>
+      </View>
     );
   }
 }
