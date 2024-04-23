bd443a5c56638f20c956673a6dca7221e77b83f4
Tiago Fernandes Ribeiro <89404933+TiagoFernandes11@users.noreply.github.com>
2024-04-23 16:53:06 -0300

    flag component 1/2

--- ProjetoCampoMinado/src/components/Field.js
+++ ProjetoCampoMinado/src/components/Field.js
@@ -2,13 +2,15 @@ import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 import params from "../params";
 import Mine from "./Mine";
+import Flag from "./Flag";
 
 export default function Field(props) {
-  const { mined, opened, nearMines, exploded } = props;
+  const { mined, opened, nearMines, exploded, flagged } = props;
 
   const styleField = [styles.field];
   if (opened) styleField.push(styles.opened);
   if (exploded) styleField.push(styles.exploded);
+  if (flagged) styleField.push(styles.flagged, styles.regular);
   if (styleField.length === 1) styleField.push(styles.regular);
 
   let color = null;
@@ -27,6 +29,7 @@ export default function Field(props) {
         false
       )}
       {mined && opened ? <Mine /> : false}
+      {flagged && !opened ? <Flag /> : false}
     </View>
   );
 }
@@ -56,6 +59,6 @@ const styles = StyleSheet.create({
   },
   exploded: {
     backgroundColor: "red",
-    borderColor:"red",
+    borderColor: "red",
   },
 });
